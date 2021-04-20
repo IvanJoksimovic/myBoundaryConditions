20c98d0935b2c6fbe7ff055bf776dfacef7061ec
Ivan Joksimovic <CSI\ij29culu@wmpc149.csi.tu-darmstadt.de>
2021-04-20 10:00:00 +0200

    First commit - all working

---
+++ fixedMapFvPatchField.C
@@ -0,0 +1,161 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2012-2016 OpenFOAM Foundation
+    Copyright (C) 2017-2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fixedMapFvPatchField.H"
+#include "volFields.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class Type>
+Foam::fixedMapFvPatchField<Type>::fixedMapFvPatchField
+(
+    const fvPatch& p,
+    const DimensionedField<Type, volMesh>& iF
+)
+:
+    fixedValueFvPatchField<Type>(p, iF),
+    meanValue_()
+{}
+
+
+template<class Type>
+Foam::fixedMapFvPatchField<Type>::fixedMapFvPatchField
+(
+    const fvPatch& p,
+    const DimensionedField<Type, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    fixedValueFvPatchField<Type>(p, iF, dict),
+    meanValue_(Function1<Type>::New("meanValue", dict))
+{
+    this->patchType() = dict.getOrDefault<word>("patchType", word::null);
+}
+
+
+template<class Type>
+Foam::fixedMapFvPatchField<Type>::fixedMapFvPatchField
+(
+    const fixedMapFvPatchField<Type>& ptf,
+    const fvPatch& p,
+    const DimensionedField<Type, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    fixedValueFvPatchField<Type>(ptf, p, iF, mapper),
+    meanValue_(ptf.meanValue_.clone())
+{}
+
+
+template<class Type>
+Foam::fixedMapFvPatchField<Type>::fixedMapFvPatchField
+(
+    const fixedMapFvPatchField<Type>& ptf
+)
+:
+    fixedValueFvPatchField<Type>(ptf),
+    meanValue_(ptf.meanValue_.clone())
+{}
+
+
+template<class Type>
+Foam::fixedMapFvPatchField<Type>::fixedMapFvPatchField
+(
+    const fixedMapFvPatchField<Type>& ptf,
+    const DimensionedField<Type, volMesh>& iF
+)
+:
+    fixedValueFvPatchField<Type>(ptf, iF),
+    meanValue_(ptf.meanValue_.clone())
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class Type>
+void Foam::fixedMapFvPatchField<Type>::updateCoeffs()
+{
+    if (this->updated())
+    {
+        return;
+    }
+
+    //const polyPatch& thisPatch = this->patch().patch(); //hshs(); //.boundaryMesh().patches(); //.mesh().boundary().patches();
+
+    wordList patchNames = this->patch().patch().boundaryMesh().names();
+
+
+
+
+    if(Pstream::parRun())
+    {
+    	//std::cout<<"\n UPDATING on processor:" << Pstream::myProcNo() << std::endl;
+    	forAll(patchNames,i)
+    	{
+    		std::cout << "\n On processor " << Pstream::myProcNo() <<" , patchName[ " << i <<"]: " << patchNames[i] << "\n"<<endl;
+    	}
+
+
+    	
+    	//PtrList<boundaryPatch> patches = this->patch().boundaryMesh().patches();
+    }
+
+    const scalar t = this->db().time().timeOutputValue();
+    Type meanValue = meanValue_->value(t);
+
+    Field<Type> newValues(this->patchInternalField());
+
+    Type meanValuePsi =
+        gSum(this->patch().magSf()*newValues)
+       /gSum(this->patch().magSf());
+
+    if (mag(meanValue) > SMALL && mag(meanValuePsi)/mag(meanValue) > 0.5)
+    {
+        newValues *= mag(meanValue)/mag(meanValuePsi);
+    }
+    else
+    {
+        newValues += (meanValue - meanValuePsi);
+    }
+
+    this->operator==(newValues);
+
+    fixedValueFvPatchField<Type>::updateCoeffs();
+}
+
+
+template<class Type>
+void Foam::fixedMapFvPatchField<Type>::write(Ostream& os) const
+{
+    fvPatchField<Type>::write(os);
+    meanValue_->writeData(os);
+    this->writeEntry("value", os);
+}
+
+
+// ************************************************************************* //
---
+++ lnInclude/fixedMapFvPatchFields.C
@@ -0,0 +1,1 @@
+../fixedMapFvPatchFields.C
